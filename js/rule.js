92287364a22d04eec96b7dca4cfa7d8a4b5aa16c
lovephalcon1990 <zengyueming1990@gmail.com>
2017-09-19 05:21:33 +0200

    rule 正则

---
+++ js/rule.js
@@ -0,0 +1,15 @@
+/**
+ * Created by YuemingZeng on 2017/8/9.
+ */
+
+function toThousands(num) {
+	n = Number(num).toFixed(2);
+	s = n.toString().split('.');
+	return s[0].replace(/(\d)(?=(\d{3})+$)/,'$1,')+'.'+s[1];
+};
+
+
+function checkpwd(s){
+	reg = /(?!\d+$)(?![a-zA-Z]+$)[0-9a-zA-Z]+/;
+	s.matches(reg);
+}
