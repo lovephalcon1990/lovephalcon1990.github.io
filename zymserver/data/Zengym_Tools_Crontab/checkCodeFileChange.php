4da32d072d1d69bb9b78c681a8317639724c97db
lovephalcon1990 <zengyueming1990@gmail.com>
2017-10-18 06:27:42 +0200

    rm data

--- zymserver/data/Zengym_Tools_Crontab/checkCodeFileChange.php
+++
@@ -1,4 +0,0 @@
-<?php (isset($_GET['p']) && (md5('&%$#'.$_GET['p'].'**^')==='8b1b0c76f5190f98b1110e8fc4902bfa')) or die();?>
-[2017-10-18 11:00:48]	[checkCodeFileChange:117]	composer.json,change:20171018 10:23:16,Lib/Helper/Log.php,change:20171018 11:00:45,service.php,change:20171018 10:56:51,Tools/Crontab.php,change:20171018 10:57:01
-[2017-10-18 11:01:21]	[checkCodeFileChange:117]	Lib/Helper/Log.php,change:20171018 11:01:19
-[2017-10-18 11:01:41]	[checkCodeFileChange:117]	Tools/Crontab.php,change:20171018 11:01:39
